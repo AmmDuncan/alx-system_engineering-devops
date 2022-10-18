f99bdb35c0ef6f06a47d7f9c971f92604a565fbc
ammduncan <ammielgyanyawson@gmail.com>
2022-10-18 16:01:54 +0000

    fix: 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/((?<=from:).+?(?=\]))|((?<=to:).+?(?=\]))|((?<=flags:).+?(?=\]))/).select { |c| c }.join(',').scan(/[a-z0-9A-Z\-\:]+\,?/).join
+puts ARGV[0].scan(/((?<=from:).+?(?=\]))|((?<=to:).+?(?=\]))|((?<=flags:).+?(?=\]))/).select { |c| c }.join(',').scan(/[^,]+\,?/).join
