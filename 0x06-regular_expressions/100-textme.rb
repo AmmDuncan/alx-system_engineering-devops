310b458eda805cc81bfda9578b81e71fa8af824a
ammduncan <ammielgyanyawson@gmail.com>
2022-10-18 16:02:25 +0000

    fix 8: hacky

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/((?<=from:).+?(?=\]))|((?<=to:).+?(?=\]))|((?<=flags:).+?(?=\]))/).select { |c| c }.join(',').scan(/[^,]+\,?/).join
+puts ARGV[0].scan(/((?<=from:).+?(?=\]))|((?<=to:).+?(?=\]))|((?<=flags:).+?(?=\]))/).join(',').scan(/[^,]+\,?/).join
