9f2d8c56d588112f11dc3274bba51b1182dddfd8
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-12-13 01:02:55 -0800

    fix 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in tmp folder with certain properties
 file { '/tmp/school':
-  path  => '/tmp/school',
-  mode  => "0744",
-  owner  => 'www-data',
-  group    => 'www-data',
-  ensure    => file,
-  content    => 'I love Puppet'
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  path    => '/tmp/school',
+  content => 'I love Puppet'
 }
