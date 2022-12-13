74dca169f34d4a3dc02d83086da93f4620644947
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-12-13 00:37:04 -0800

    fix 20

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file in tmp folder with certain properties
 file { '/tmp/school':
   path  => '/tmp/school'
-  mode  => 0744
+  mode  => '0744'
   owner  => 'www-data'
   group    => 'www-data'
   ensure    => file
