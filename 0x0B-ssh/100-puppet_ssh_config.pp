5c51b44ff3e113f8255c61d1d075d8cbf6b2d87d
Ammiel Yawson <53538084+AmmDuncan@users.noreply.github.com>
2022-12-14 08:34:23 +0000

    fix 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,17 @@
-# Configure ssh client
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  content => "Host 54.146.83.42
-	  User ubuntu
-	  IdentityFile ~/.ssh/school
-	  IdentityOnly yes\n"
+# Setting up client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+
+}
+
+file_line { 'Declare Identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  replace => true,
 }
